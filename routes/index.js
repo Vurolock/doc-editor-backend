5f91ede2e858dacae5e32a68c703507aaa58bb0d
Andrew Keller <Keller192@gmail.com>
2018-02-08 10:30:21 -0500

    changed port to 4000, added index route that responds with JSON when it recieves a GET request

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Note = require('../models/note');
 
 router.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -8,6 +9,12 @@ router.all('*', (req, res, next) => {
   next();
 })
 
-router.get('/', function(req, res, next) {});
+router.route('/')
+  .get((req, res) => {
+    Note.findAll()
+      .then(allNotes => {
+        res.json(allNotes);
+      });
+  });
 
 module.exports = router;
