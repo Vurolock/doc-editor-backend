5671e155a94ad28f360937a0215cc5890aa60cfc
Andrew Keller <Keller192@gmail.com>
2018-02-08 15:10:12 -0500

    orders notes by id on GET requests, adds PUT request for editing existing notes, begins DELETE request

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,50 @@ const router = express.Router();
 const Note = require('../models/note');
 
 router.all('*', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  next();
+  	res.header("Access-Control-Allow-Origin", "*");
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  	next();
 })
 
 router.route('/')
-  .get((req, res) => {
-    Note.findAll()
-      .then(allNotes => {
-        res.json(allNotes);
-      });
-  })
-  .post((req, res) => {
-    console.log(req.body);
-    Note.create({
-      title: req.body.title,
-      content: req.body.content
-    }).then(() => {
-      // responds with error 201 'create' since there is no render or redirect to respond with
-      res.status(201).send();
-    }).catch((error) => {
-      console.log(error);
-    })
-  });
+  	.get((req, res) => {
+    	Note.findAll({
+			order: [['id', 'ASC']]
+		}).then(allNotes => {
+			// responds with all notes
+        	res.json(allNotes);
+      	});
+  	})
+  	.post((req, res) => {
+    	Note.create({
+      		title: req.body.title,
+      		content: req.body.content
+    	}).then((newNote) => {
+      		// responds with new note
+      		res.json(newNote);
+    	});
+  	})
+  	.put((req, res) => {
+		Note.findOne({
+			where: {id: req.body.id}
+		}).then(note => {
+			note.updateAttributes({
+				title: req.body.title,
+				content: req.body.content
+			}).then((editedNote) => {
+				// responds with edited note
+				res.json(editedNote)
+			});
+		});
+  	})
+	.delete((req, res) => {
+		Note.destroy({
+			// delete here
+		}).then((deletedNote) => {
+			// responds with deleted note
+			res.json(deletedNote)
+		});
+	});
 
 module.exports = router;
